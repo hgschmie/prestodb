98b520146819067505ca16da950e805d22d4e359
Henning Schmiedehausen <henning@groupon.com>
2014-06-18 11:14:57 -0700

    add lost test back

---
+++ presto-kafka/src/test/java/com/facebook/presto/kafka/TestKafkaPlugin.java
@@ -0,0 +1,48 @@
+package com.facebook.presto.kafka;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.util.List;
+
+import com.facebook.presto.spi.Connector;
+import com.facebook.presto.spi.ConnectorFactory;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.TypeManager;
+import com.google.common.collect.ImmutableMap;
+
+import org.testng.annotations.Test;
+
+public class TestKafkaPlugin
+{
+    private static final TypeManager DUMMY_TYPE_MANAGER = new TypeManager()
+    {
+        @Override
+        public Type getType(String typeName)
+        {
+            return null;
+        }
+    };
+
+    @Test
+    public ConnectorFactory testConnectorExists()
+    {
+        KafkaPlugin plugin = new KafkaPlugin();
+        plugin.setTypeManager(DUMMY_TYPE_MANAGER);
+
+        List<ConnectorFactory> factories = plugin.getServices(ConnectorFactory.class);
+        assertNotNull(factories);
+        assertEquals(factories.size(), 1);
+        ConnectorFactory factory = factories.get(0);
+        assertNotNull(factory);
+        return factory;
+    }
+
+    @Test
+    public void testSpinup()
+    {
+        ConnectorFactory factory = testConnectorExists();
+        Connector c = factory.create("test-connector", ImmutableMap.<String, String>of("node.environment", "test"));
+        assertNotNull(c);
+    }
+}
